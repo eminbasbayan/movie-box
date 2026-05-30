79f6f6de45da10a0925a5a7ef95f0b1e3691a2ff
eminbasbayan <emin.basbayan@icloud.com>
2026-05-30 13:30:32 +0300

    expo router tabs kullanımı

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,37 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{}}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Ana Sayfa',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Ara',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          title: 'Favoriler',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/search.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+function SearchScreen() {
+  return <Text> Search SearchScreenScreen</Text>;
+}
+
+export default SearchScreen;
