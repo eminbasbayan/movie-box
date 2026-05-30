f0b8dbf759dfcfe6977455a1432a9191b566b829
eminbasbayan <emin.basbayan@icloud.com>
2026-05-30 10:42:23 +0300

    fonts dosyası

---
+++ constants/fonts.js
@@ -0,0 +1,19 @@
+const Fonts = {
+  sizes: {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
+    xxl: 22,
+    title: 28,
+  },
+  weights: {
+    regular: '400',
+    medium: '500',
+    semibold: '600',
+    bold: '700',
+  },
+};
+
+export default Fonts;
