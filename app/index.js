2ddff2b448e2f10c4f4f798ec9e00bb84d895f4d
eminbasbayan <emin.basbayan@icloud.com>
2026-05-30 12:54:27 +0300

    SafeAreaView kullanımı

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
 import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 function HomeScreen(){
-    return <Text>Hello!</Text>
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <Text>Hello!</Text>
+        </SafeAreaView>
+    )
 }
 
 export default HomeScreen
