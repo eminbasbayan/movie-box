d38355154ebdd4ee8a982b304e2622096fe78141
eminbasbayan <emin.basbayan@icloud.com>
2026-06-03 15:26:32 +0300

    search ekran FlatList

--- app/(tabs)/search.js
+++ app/(tabs)/search.js
@@ -1,7 +1,52 @@
-import { Text } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useTheme } from '../../context/ThemeContext';
+import Fonts from '../../constants/fonts';
+import MovieCard from '../../components/MovieCard';
 
 function SearchScreen() {
-  return <Text> Search SearchScreenScreen</Text>;
+  const { colors } = useTheme();
+  return (
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors.background }]}
+    >
+      <Text style={[styles.title, { color: colors.text }]}>Ara</Text>
+
+      <FlatList
+        data={[
+          { id: 1, name: 'Movie 1' },
+          { id: 2, name: 'Movie 2' },
+          { id: 3, name: 'Movie 3' },
+        ]}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        contentContainerStyle={styles.contenContainer}
+        contentContainerStyle={styles.listContent}
+        columnWrapperStyle={styles.row}
+        ListHeaderComponent={<View style={styles.header}></View>}
+        renderItem={({ item }) => <MovieCard horizontal />}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: Fonts.sizes.title,
+    fontWeight: Fonts.weights.bold,
+    paddingHorizontal: 16,
+    paddingTop: 8,
+  },
+  listContent: {
+    paddingHorizontal: 16,
+    paddingBottom: 20,
+  },
+  row: {
+    justifyContent: 'space-between',
+  },
+});
 
 export default SearchScreen;
